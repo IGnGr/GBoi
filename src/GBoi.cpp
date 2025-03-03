ef9f95442620fb9feda4fd7c11b3eb406ef4d77f
frapama6 <izgngr@gmail.com>
2025-03-04 00:16:29 +0100

    SDL Hello World

---
+++ src/GBoi.cpp
@@ -0,0 +1,12 @@
+﻿// GBoi.cpp : Defines the entry point for the application.
+//
+
+#include "GBoi.h"
+
+using namespace std;
+
+int main()
+{
+	SDL_Log("Hello, World!");
+	return 0;
+}
---
+++ src/GBoi.h
@@ -0,0 +1,7 @@
+﻿// GBoi.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <SDL3/SDL.h>
+
